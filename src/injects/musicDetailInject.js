ed10897f7811231ca47fa7b5fc03619bbcafea07
nyraa <112930946+nyraa@users.noreply.github.com>
2024-05-14 03:41:57 +0800

    add music detail inject viewmore action

--- src/injects/musicDetailInject.js
+++ src/injects/musicDetailInject.js
@@ -6,7 +6,7 @@ Router.register(/\/musicDetail\/$/, (req, html) => {
     const $ = cheerio.load(html);
     const musicIdentifier = {
         coverID: $("img.w_180.m_5.f_l").attr("src").split("/").pop().split(".")[0],
-        type: $(".basic_block .f_l.h_20").attr("src")?.split("/")?.pop()?.split(".")?.[0]?.split("_")?.[1] ?? "utage",
+        kind: $(".basic_block .f_l.h_20").attr("src")?.split("/")?.pop()?.split(".")?.[0]?.split("_")?.[1] ?? "utage",
         utageKinds: $(".music_kind_icon_utage_text_detail").map((i, e) => {
             const element = $(e);
             return element.text();
@@ -32,6 +32,41 @@ Router.register(/\/musicDetail\/$/, (req, html) => {
                 }
                 viewmore_loading = true;
                 const xhr = new XMLHttpRequest();
+                xhr.open("POST", "/extend/recorddata/");
+                xhr.setRequestHeader("Content-Type", "application/json");
+                xhr.onreadystatechange = () => {
+                    if(xhr.readyState === 4 && xhr.status === 200)
+                    {
+                        const data = JSON.parse(xhr.responseText);
+                        const formatter = new Intl.DateTimeFormat("ja", {
+                            timeZone: "Asia/Tokyo",
+                            year: "numeric",
+                            month: "2-digit",
+                            day: "2-digit",
+                            hour: "2-digit",
+                            minute: "2-digit",
+                            hour12: false
+                        });
+                        for(let record of data.data)
+                        {
+                            ${recordDOMString}
+                            $(\`#\${level}_viewmore_btn\`).before(domstring);
+                        }
+                        viewmore_offset[level] = data.next;
+                        if(data.next === null)
+                        {
+                            $(\`#\${level}_viewmore_btn\`).remove();
+                        }
+                        viewmore_loading = false;
+                    }
+                };
+                xhr.onerror = () => {
+                    viewmore_loading = false;
+                };
+                xhr.send(JSON.stringify({
+                    offset: viewmore_offset[level],
+                    musicIdentifier: ${JSON.stringify(musicIdentifier)}
+                }));
             }
         </script>
     `);
@@ -41,7 +76,7 @@ Router.register(/\/musicDetail\/$/, (req, html) => {
         const level = element.attr("class").split(" ").find((c) => c.includes("music_")).split("_")[1];
         const logBlockDOMString = `
             <div id="${level}_log_block">
-                <button type="button" onclick="javascript:viewmore('${level}')" class="m_10">
+                <button id="${level}_viewmore_btn" type="button" onclick="javascript:viewmore('${level}')" class="m_10">
                     <img src="https://maimaidx-eng.com/maimai-mobile/img/btn_more.png" class="w_84" />
                 </button>
             </div>
