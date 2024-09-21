40cc04c3504241a91e540edf3dab5f12be6ec152
nyraa <112930946+nyraa@users.noreply.github.com>
2024-09-21 08:56:00 +0800

    fix score analysis not inject when record miss

--- src/injects/playlogDetailInject.js
+++ src/injects/playlogDetailInject.js
@@ -7,21 +7,6 @@ import { getCacheFileSync } from "../cache.js";
 Router.register(/\/record\/playlogDetail\//, (req, html) => {
     const $ = cheerio.load(html);
 
-    // get play time as record id
-    const datetime = new Date($(".sub_title span:not(.red)").text().trim() + " GMT+0900");
-    const recordIdInt = datetime.getTime();
-
-    const record = db.chain.get("records").find((r) => new Date(r.datetime).getTime() === recordIdInt).value();
-    if(!record)
-    {
-        console.log(`Record miss ${datetime.toDateString()}`);
-        return $.html();
-    }
-    
-    // inject achievement diff
-    const diffDomString = `<span class="f_10" style="display: block;">${record.achievementDiff >= 0 ? "+" : "-"}${record.achievementDiff.toFixed(4)}%</span>`;
-    $(".playlog_achievement_txt>.f_20").after(diffDomString);
-
     // inject chievement analysis
     $("body").append(`
         <script language="javascript">
@@ -33,5 +18,22 @@ Router.register(/\/record\/playlogDetail\//, (req, html) => {
             ${getCacheFileSync("src/injects/client/achievementLostAnalysis.css")}
         </style>
     `);
+
+    // get play time as record id
+    const datetime = new Date($(".sub_title span:not(.red)").text().trim() + " GMT+0900");
+    const recordIdInt = datetime.getTime();
+
+    // inject achievement diff
+    const record = db.chain.get("records").find((r) => new Date(r.datetime).getTime() === recordIdInt).value();
+    if(!record)
+    {
+        console.log(`Record miss ${datetime.toDateString()}`);
+    }
+    else
+    {
+        const diffDomString = `<span class="f_10" style="display: block;">${record.achievementDiff >= 0 ? "+" : "-"}${record.achievementDiff.toFixed(4)}%</span>`;
+        $(".playlog_achievement_txt>.f_20").after(diffDomString);
+    }
+
     return $.html();
 });
